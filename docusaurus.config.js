397a41ba6ae7199abc597f53441c5d48b5d80741
Alfredo Cedeno <ajcborges@gmail.com>
2023-04-22 07:52:59 +0000

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,153 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Alfredo Cedeno',
+  tagline: 'Hosting your own cloud infrastructure is cool',
+  url: 'https://alfredo.cedeno.me',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'throw',
+  favicon: 'img/favicon.ico',
+  organizationName: 'ajcborges', 
+  projectName: 'website', 
+  trailingSlash: true,
+
+  
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/ajcborges/website/tree/main/alfredo.cedeno.me',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/ajcborges/website/tree/main/alfredo.cedeno.me',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'Alfredo Cedeno',
+        logo: {
+        //  alt: 'My Site Logo',
+          src: 'img/code-tech-dev-svgrepo-com.svg',
+        },
+        items: [
+          /**{
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Tutorial',
+          },*/
+          {
+            type: 'doc',
+            docId: 'intro',
+            label: 'Docs',
+            position: 'left',            
+          },
+          { to: '/blog', 
+            label: 'Blog', 
+            position: 'left'
+          },
+          /**{
+            to: '/about', 
+            label: 'About', 
+            position: 'left', 
+          },  */        
+          {
+            href: 'https://github.com/ajcborges',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          /**{
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },*/
+          {
+            title: 'Blogs',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'Medium',
+                href: 'https://ajcborges.medium.com/',
+              },
+            ],
+          },          
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/users/11536667/alfredo-cedeno?tab=profile',
+              },
+              {
+                label: 'LinkedIn',
+                href: 'https://www.linkedin.com/in/alfredocedeno/',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/ajcborges',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'My Github',
+                href: 'https://github.com/ajcborges',
+              },
+              {
+                label: 'This website (as code)',
+                href: 'https://github.com/ajcborges/website',
+              },
+              {
+                label: 'Professional Development Plan (Private)',
+                href: 'https://github.com/ajcborges/pdp/projects/1',
+              },
+            ],
+          },          
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Alfredo Cedeno. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: 'Professional focus',
+    Svg: require('@site/static/img/focus-add-svgrepo-com.svg').default,
+    description: (
+      <>
+        Designing and implementing creative solutions in Google Cloud Platform (GCP) 
+        using Infrastructure-as-Code (IaC) tooling and Docker, making them more efficient 
+        and cost-effective, while also ensuring are adaptable to future changes and growth.
+      </>
+    ),
+  },
+  {
+    title: 'Driven by',
+    Svg: require('@site/static/img/dev-to-svgrepo-com.svg').default,
+    description: (
+      <>
+        Taking pride in my ability to plan and maintain various security settings, 
+        and committed to continuous education and research to stay up-to-date 
+        on industry trends and technologies.
+      </>
+    ),
+  },
+  {
+    title: 'About Me',
+    Svg: require('@site/static/img/user-svgrepo-com.svg').default,
+    //Svg: require('@site/static/img/about_me.jpeg').default,
+    description: (
+      <>
+        DevSecOps Engineer with a passion for developing and implementing effective 
+        security strategies, building strong teams, and safeguarding company information. 
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
