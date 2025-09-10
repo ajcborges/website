7b6498006f72a1200636271b659dda701934bca2
alfredo cedeno <ajcborges@gmail.com>
2025-09-11 09:49:55 +1000

    fix typo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  //title: 'Alfredo Cedeno',
+  title: 'Alfredo Cedeno',
   tagline: 'Cloud Security Engineer - Melbourne, Australia',
   favicon: 'img/favicon.ico',
 
