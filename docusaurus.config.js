4e99159264641c7118b26709db69f2f5ccedfa2a
alfredo cedeno <ajcborges@gmail.com>
2025-09-11 09:53:29 +1000

    update tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Alfredo Cedeno',
-  tagline: 'Cloud Security Engineer - Melbourne, Australia',
+  tagline: 'Cloud Security Engineer - Photographer - HomeLab Enthusiast',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
