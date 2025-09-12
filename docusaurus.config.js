12a7e73534bba7a34ad2197983ff6c4c9beaad72
alfredo cedeno <ajcborges@gmail.com>
2025-09-12 12:01:36 +1000

    missing comma

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -103,7 +103,7 @@ const config = {
         highlightSearchTermsOnTargetPage: true, // replaces enableHighlight
       },
     ],
-  ]
+  ],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
